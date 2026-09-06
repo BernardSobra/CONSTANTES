9706557ce8102833e27132695a0de6afed676e56
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-06 19:07:03 +0200

    Mise en forme des tests des fonction de dessin

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d425da8138b2da9b1ba3a42c53cdccf17f4207c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c405215ea96617f3ffee9c314c84343268f7dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
