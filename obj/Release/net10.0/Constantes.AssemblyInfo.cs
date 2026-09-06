9f538234929cc7f43301c05495fc63e7484d281a
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-06 11:43:37 +0200

    Validation des tests

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea3acafeaaa85f7d4a126cdccad581392f18ac1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c301fb68ad95e2704fafe3e0bfc9122e500805")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
