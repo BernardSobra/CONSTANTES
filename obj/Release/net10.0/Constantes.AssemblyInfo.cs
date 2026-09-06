bea3acafeaaa85f7d4a126cdccad581392f18ac1
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-06 09:37:49 +0200

    Fin du codage d'image, trois procédures ont été volontairement négligée

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6843652e285868fd47d3f5213800f3fbf5c46c1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589ee125ac3f3dcd1d2ab92a5347856055b600d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
