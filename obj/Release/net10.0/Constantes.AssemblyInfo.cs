715560ea92b9264ecb555d2be363c1030991ab5f
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-07 10:57:42 +0200

    Fin des fonction de Texte

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+323ada659616857c095616a15975d556bee45a48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e91ffae307404562a573d276ba72b3c7f8c737f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
