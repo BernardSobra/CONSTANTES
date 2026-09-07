69510d1b340330bd8385c6742626a8ebc601e1fa
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-07 16:46:52 +0200

    Deux fonctions date à coder et à tester : fait !

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+715560ea92b9264ecb555d2be363c1030991ab5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+657e6577d9d6f12e350e0cae8a01386030c5295d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
