1fc7f0a779e6362f2e2aaa49842f738d622d5c6a
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-08 18:16:29 +0200

    Fin des numérique en codant Conversion

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10cddff6f3b19d28599dd14e5550687c054c91e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26479e956d119462534391557e4ac8761c3a70cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
