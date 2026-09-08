b3bb1ecd51f46565a4c5e9c750fa3ccded6d1ed4
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-08 10:16:29 +0200

    Fonctions Systeme, conitnuation.

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69510d1b340330bd8385c6742626a8ebc601e1fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f52ce26e0bf8d2b7c5c42df4cb34ef5be3b14ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
