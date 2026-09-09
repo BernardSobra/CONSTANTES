02d3584fb600e85288e4a33129ae6a103cf322f8
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-09 17:09:17 +0200

    Fonctions du Navigateur

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1cf09674c96ccde7aba5a8a7623eb32fa8c76a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+864481f0d77fd86406e42aaffb8c198f2c740614")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
