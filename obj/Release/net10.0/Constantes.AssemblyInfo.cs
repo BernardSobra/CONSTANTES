864481f0d77fd86406e42aaffb8c198f2c740614
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-09 15:45:27 +0200

    Première fonctions de F_Navigateur.cs

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a8c1b5103a1e9c3266665ff9f50fdf416d598cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1cf09674c96ccde7aba5a8a7623eb32fa8c76a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
