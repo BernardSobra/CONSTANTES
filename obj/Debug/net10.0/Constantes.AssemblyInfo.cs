09934f35fb82a6f15993a950e7d34951f67be53c
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-10 07:31:28 +0200

    Premières fonctions BTLE et UPNP.

--- obj/Debug/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Debug/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a14419797d5691c8789d9f5974b4ad42518b439")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def6eddf37501bda70f2b6d18177756b275a1b41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
