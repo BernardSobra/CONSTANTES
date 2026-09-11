a2a3763df44cceee5c3cb8260ad2a24616164eed
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-11 04:45:04 +0200

    Fonction F_Btle mise en conformité avec la nouvelle classe TA_BTLE

--- obj/Debug/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Debug/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5548fcbf038a0bf3a7fbdd7d64eff618467143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd1ca39bec994d137f229e349b161266eb8c0e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
