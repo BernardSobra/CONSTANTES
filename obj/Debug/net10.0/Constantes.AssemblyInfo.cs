f18e02d667042ead500b946733b1a175b2f917d6
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-13 10:19:27 +0200

    Codage de quelques types avancés supplémentaires (URI, CglDoc...)

--- obj/Debug/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Debug/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a48833feeb5375a7817d8a04cd3dada0707f65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b6d35f85812d595864a6570ee0d89e84b7cd3cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
