225a97ebe69b5fbebf580e53ef4c064ace5bf8bb
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-14 18:46:08 +0200

    Fin de F_Documents. LA lcasse et ses fonctons sont désormais terminées.

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee387773a2d5101ca984ce8d5dcb2ebb26007726")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651159ffc68a618dfd1f4dcb43c70d3fa5cb8546")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
