1b5c353862b668c5bc7ff4ba3e3fcf822fe9f0a1
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-15 14:29:26 +0200

    Sérialise/Désérialise en syntaxe préfixée, noms anglais vérifiés
    - Liste, Pile, File : Sérialise/Désérialise (+ Serialize/Deserialize)
      en syntaxe préfixée ; ListeSérialise, PileSérialise, FileSérialise
      et leurs Désérialise supprimées avec leurs alias anglais
    - Tests en aller-retour français, anglais et croisé
    - F_Anglais : mentions « non confirmé » vérifiées contre la doc PC SOFT,
      alias devises (préfixe e), Drive partagés et nouveautés 2026 renommés

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5e895662d369926f846df6aebaa8aaed53e288")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3927d32bf815bbfc3bbf846d531eedb72505f938")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
