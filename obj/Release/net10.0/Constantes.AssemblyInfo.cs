3927d32bf815bbfc3bbf846d531eedb72505f938
Bernard Sobra <bernard.sobra@gmail.com>
2026-09-15 09:40:15 +0200

    Mise en forme des versions anglaises

--- obj/Release/net10.0/Constantes.AssemblyInfo.cs
+++ obj/Release/net10.0/Constantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8e77848a4aa90ef43febbd603824ad19d13b0ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5e895662d369926f846df6aebaa8aaed53e288")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
